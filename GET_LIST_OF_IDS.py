4fcb8494001341d5ec68973eb059e4981fab5f2c
CyCodeDev <36279320+CyCodeDev@users.noreply.github.com>
2019-03-10 21:46:17 -0700

    Update GET_LIST_OF_IDS.py

--- GET_LIST_OF_IDS.py
+++ GET_LIST_OF_IDS.py
@@ -1,8 +1,9 @@
 from XML_PARSE_CLASS_MAIN import xmlparse
+fileName = input("Enter a file name: ")
 
-
-a = xmlparse('gsm_update_etd_APINVCLD_GSMF00O.16.1_1.20180417T1800-04.xml')
+a = xmlparse(fileName)
+# gsm_update_etd_APINVCLD_GSMF00O.16.1_1.20180417T1800-04.xml
 
 with open('OPTIONS_ID_List_1.txt', 'w') as f:
-	I = list(a.bigI())
-	f.write(str(I))
+	I = list(a.bigI()) # BigI method get the list of Ids
+	f.write(str(I)) #
